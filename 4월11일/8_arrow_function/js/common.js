e1e2ec7a89c8a1a954fac9eee9a9aa31de18aed0
2songyi <5469011s2@daum.net>
2022-04-11 22:34:02 +0900

    test

--- 4월11일/8_arrow_function/js/common.js
+++ 4월11일/8_arrow_function/js/common.js
@@ -6,22 +6,3 @@
 let commonFunc = function(){
   console.log('일반표기함수 입니다.')
 }
-
-// 선언적 함수
-function numFunc(){
-
-}
-
-// arrow function 화살표함수
-// ES6이상 사용
-let arrowFunc = () => {
-  console.log('화살표함수 입니다.')
-}
-
-commonFunc();
-arrowFunc();
-
-// 즉시실행함수
-(function(){
-  console.log('즉시실행함수')
-})(); //따로 함수 호출하지 않아도됨
